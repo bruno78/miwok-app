77a9ea239ad8ebc6724501715b3e60750be5ddab
bruno78 <devbruno78@gmail.com>
2018-02-05 16:16:26 -0500

    Add imageResource to Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,6 +7,7 @@ package com.example.android.miwok;
 public class Word {
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageResourceId;
 
     /**
      * This constructor accepts two parameters, first is the Miwok word and second is the translation
@@ -17,6 +18,21 @@ public class Word {
         mMiwokTranslation = miwokWord;
         mDefaultTranslation = defaultWord;
     }
+
+    /**
+     * This constructor accepts three parameters, first is the Miwok word, second is the translation
+     * and third is Image Resource Id to locate image.
+     * @param miwokWord to be translated
+     * @param defaultWord is the translation of the Miwok word
+     * @param vImageResourceId is the id resource of the image
+     */
+    public Word(String defaultWord, String miwokWord, int vImageResourceId) {
+        mMiwokTranslation = miwokWord;
+        mDefaultTranslation = defaultWord;
+        mImageResourceId = vImageResourceId;
+    }
+
+
 
     /**
      * Get the default translation of the word.
@@ -31,5 +47,10 @@ public class Word {
     public String getMiwokTranslation(){
         return mMiwokTranslation;
     }
+
+    /**
+     * Get the id for the image resource location
+     */
+    public int getmImageResourceId() { return mImageResourceId; }
 
 }
