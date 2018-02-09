0ed4528f868ce11cc60d067e02827fd151fc74c3
Bruno G. Tavares <devbruno78@gmail.com>
2018-02-08 20:02:37 -0500

    Add audio manager for Colors Activity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 public class ColorsActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
 
     /**
      * This event listener gets triggered when audio file finishes playing.
@@ -22,6 +25,32 @@ public class ColorsActivity extends AppCompatActivity {
         @Override
         public void onCompletion(MediaPlayer mp) {
             releaseMediaPlayer();
+        }
+    };
+
+    /**
+     * This event listener gets triggered when audio states change
+     */
+    private AudioManager.OnAudioFocusChangeListener mAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+
+                mMediaPlayer.pause();
+                // when resumes, goes back to the beginning of the audio, so the user can listen
+                // to the audio again.
+                mMediaPlayer.seekTo(0);
+
+            }
+            else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+
+                releaseMediaPlayer();
+            }
+            else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+
+                mMediaPlayer.start();
+            }
         }
     };
 
@@ -49,6 +78,9 @@ public class ColorsActivity extends AppCompatActivity {
         colorVocabulary.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow,
                                                                                           R.raw.color_mustard_yellow));
 
+        // Get Audio Manager system service from the context
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         WordAdapter adapter = new WordAdapter(this, colorVocabulary, R.color.category_colors);
 
         ListView colorListView = (ListView) findViewById(R.id.list);
@@ -59,9 +91,15 @@ public class ColorsActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 releaseMediaPlayer();
                 Word word = (Word) parent.getItemAtPosition(position);
-                mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceId());
-                mMediaPlayer.start();
-                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+
+                int result = mAudioManager.requestAudioFocus(mAudioFocusChangeListener, AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
 
             }
         });
@@ -83,6 +121,9 @@ public class ColorsActivity extends AppCompatActivity {
             mMediaPlayer.stop();
             mMediaPlayer.release();
             mMediaPlayer = null;
+
+            // Abandon audio focus when playback is complete;
+            mAudioManager.abandonAudioFocus(mAudioFocusChangeListener);
         }
     }
 }
