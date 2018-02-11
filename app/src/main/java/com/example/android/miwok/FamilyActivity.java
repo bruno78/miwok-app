09ad23b78d9d4021031b3f0af24ecc6dbd07481f
bruno78 <devbruno78@gmail.com>
2018-02-11 13:31:16 -0500

    Add Family Fragment

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -13,116 +13,14 @@ import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
-    private MediaPlayer mMediaPlayer;
-    private AudioManager mAudioManager;
-
-    /**
-     * This event listener gets triggered when audio file finishes playing.
-     */
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releaseMediaPlayer();
-        }
-    };
-
-    /**
-     * This event listener gets triggered when audio state changes
-     */
-    private AudioManager.OnAudioFocusChangeListener mAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int audioFocus) {
-
-            if(audioFocus == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                    audioFocus == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-
-                mMediaPlayer.pause();
-                mMediaPlayer.seekTo(0);
-            }
-            else if(audioFocus == AudioManager.AUDIOFOCUS_LOSS) {
-
-                releaseMediaPlayer();
-            }
-            else if(audioFocus == AudioManager.AUDIOFOCUS_GAIN) {
-                mMediaPlayer.start();
-            }
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-         final ArrayList<Word> familyVocabulary = new ArrayList<>();
-
-        familyVocabulary.add(new Word("father", "әpә", R.drawable.family_father,
-                                                                              R.raw.family_father));
-        familyVocabulary.add(new Word("mother", "әṭa", R.drawable.family_mother,
-                                                                              R.raw.family_mother));
-        familyVocabulary.add(new Word("son", "angsi", R.drawable.family_son,
-                                                                             R.raw.family_son));
-        familyVocabulary.add(new Word("daughter", "tune", R.drawable.family_daughter,
-                                                                                 R.raw.family_daughter));
-        familyVocabulary.add(new Word("older brother", "taachi", R.drawable.family_older_brother,
-                                                                                        R.raw.family_older_brother));
-        familyVocabulary.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother,
-                                                                                            R.raw.family_younger_brother));
-        familyVocabulary.add(new Word("older sister", "teṭe", R.drawable.family_older_sister,
-                                                                                     R.raw.family_older_sister));
-        familyVocabulary.add(new Word("younger sister", "kolliti", R.drawable.family_younger_sister,
-                                                                                          R.raw.family_younger_sister));
-        familyVocabulary.add(new Word("grandmother", "ama", R.drawable.family_grandmother,
-                                                                                   R.raw.family_grandmother));
-        familyVocabulary.add(new Word("grandfather", "paapa", R.drawable.family_grandfather,
-                                                                                     R.raw.family_grandfather));
-
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        WordAdapter adapter = new WordAdapter(this, familyVocabulary, R.color.category_family);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                releaseMediaPlayer();
-                Word word = (Word) parent.getItemAtPosition(position);
-
-                int result = mAudioManager.requestAudioFocus(mAudioFocusChangeListener, AudioManager.STREAM_MUSIC,
-                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    mMediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getAudioResourceId());
-                    mMediaPlayer.start();
-                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                }
-
-            }
-        });
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new FamilyFragment())
+                .commit();
 
 
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        // Make sure the audio stop playing when the user leaves the app
-        releaseMediaPlayer();
-    }
-
-    /**
-     * This helper method prevents multiple plays on multiple clicks.
-     */
-    private void releaseMediaPlayer() {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.stop();
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-
-            // Abandon focus when playing is done
-            mAudioManager.abandonAudioFocus(mAudioFocusChangeListener);
-        }
     }
 }
